e402de2941cd0d6ddcffe987e6812e206d89e769
Ruturaj <83021083+ruturaj-gh@users.noreply.github.com>
2022-10-15 11:07:53 +0530

    added the required program

---
+++ adding_two_numbers.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void)
+{
+    cout << "Enter two numbers" << endl;
+    int a, b;
+    cin >> a >> b;
+    cout << "The addition is " << a + b << endl;
+}
